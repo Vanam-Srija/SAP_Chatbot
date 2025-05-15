df249c9da38c72d1dd1809350ff6eab30cf9dd14
Srija Vanam <srija.vanam@ust.com>
2025-05-15 07:31:39 +0000

    Consume all entity sets in one custom api

--- srv/service.cds
+++ srv/service.cds
@@ -1,5 +1,49 @@
 using { EPM_REF_APPS_PROD_MAN_SRV as external } from './external/EPM_REF_APPS_PROD_MAN_SRV';
- 
+
 service main {
-    entity Suppliers as projection on external.Suppliers;
+  @readonly
+  function GetProductsWithSuppliers() returns array of ProductWithSupplier;
+  entity Products as projection on external.Products;
+  entity Suppliers as projection on external.Suppliers;
 }
+type SupplierDetails {
+  Id                  : String;
+  Name                : String;
+  Phone               : String;
+  Email               : String;
+  WebAddress          : String;
+  FormattedAddress    : String;
+  FormattedContactName: String;
+  ContactPhone1       : String;
+  ContactPhone2       : String;
+  ContactEmail        : String;
+}
+
+type ProductWithSupplier {
+  Id              : String;
+  Name            : String;
+  Description     : String;
+  Price           : Decimal;
+  CurrencyCode    : String;
+  ImageUrl        : String;
+  SupplierId      : String;
+  Supplier        : SupplierDetails;
+
+  StockQuantity   : Integer;
+  SubCategoryId   : String;
+  SubCategoryName : String;
+  MainCategoryId  : String;
+  MainCategoryName: String;
+  LastModified    : String;
+  DimensionHeight : String;
+  DimensionWidth  : String;
+  DimensionDepth  : String;
+  DimensionUnit   : String;
+  QuantityUnit    : String;
+  MeasureUnit     : String;
+  AverageRating   : Decimal;
+  RatingCount     : Integer;
+  WeightMeasure   : String;
+  WeightUnit      : String;
+}
+
--- srv/service.js
+++ srv/service.js
@@ -1,14 +1,82 @@
 const cds = require('@sap/cds');
- 
-module.exports = cds.server;
-module.exports=cds.service.impl(async function(){
-   
-    const product_api = await cds.connect.to('EPM_REF_APPS_PROD_MAN_SRV');
-    const { Suppliers } =this.entities;
- 
-    // Reading Material No. data fetched from S4H
-    this.on("READ", Suppliers ,(req)=>{
-        req.query.SELECT.count = false;
-        return product_api.run(req.query);
-    });
-})
+
+module.exports = cds.service.impl(async function () {
+  const epm = await cds.connect.to('EPM_REF_APPS_PROD_MAN_SRV');
+
+  this.on('GetProductsWithSuppliers', async () => {
+    // Fetch all products with all required fields
+    const products = await epm.run(
+      SELECT.from('Products').columns(
+        'Id',
+        'Name',
+        'Description',
+        'Price',
+        'CurrencyCode',
+        'ImageUrl',
+        'SupplierId',
+        'StockQuantity',
+        'SubCategoryId',
+        'SubCategoryName',
+        'MainCategoryId',
+        'MainCategoryName',
+        'LastModified',
+        'DimensionHeight',
+        'DimensionWidth',
+        'DimensionDepth',
+        'DimensionUnit',
+        'QuantityUnit',
+        'MeasureUnit',
+        'AverageRating',
+        'RatingCount',
+        'WeightMeasure',
+        'WeightUnit'
+      )
+    );
+
+    // Fetch all suppliers
+    const suppliers = await epm.run(
+      SELECT.from('Suppliers').columns(
+        'Id',
+        'Name',
+        'Phone',
+        'Email',
+        'WebAddress',
+        'FormattedAddress',
+        'FormattedContactName',
+        'ContactPhone1',
+        'ContactPhone2',
+        'ContactEmail'
+      )
+    );
+
+    const supplierMap = new Map(suppliers.map(s => [s.Id, s]));
+
+    // Map products with supplier details embedded
+    return products.map(p => ({
+      Id              : p.Id,
+      Name            : p.Name,
+      Description     : p.Description,
+      Price           : p.Price,
+      CurrencyCode    : p.CurrencyCode,
+      ImageUrl        : p.ImageUrl,
+      SupplierId      : p.SupplierId,
+      StockQuantity   : p.StockQuantity,
+      SubCategoryId   : p.SubCategoryId,
+      SubCategoryName : p.SubCategoryName,
+      MainCategoryId  : p.MainCategoryId,
+      MainCategoryName: p.MainCategoryName,
+      LastModified    : p.LastModified,
+      DimensionHeight : p.DimensionHeight,
+      DimensionWidth  : p.DimensionWidth,
+      DimensionDepth  : p.DimensionDepth,
+      DimensionUnit   : p.DimensionUnit,
+      QuantityUnit    : p.QuantityUnit,
+      MeasureUnit     : p.MeasureUnit,
+      AverageRating   : p.AverageRating,
+      RatingCount     : p.RatingCount,
+      WeightMeasure   : p.WeightMeasure,
+      WeightUnit      : p.WeightUnit,
+      Supplier        : supplierMap.get(p.SupplierId) || {}
+    }));
+  });
+});
